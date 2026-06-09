5eaa8feee7fa2ec578f28c8714f100e22b533a00
Sameerchoudhary8093 <Sameerchoudhary8093@gmail.com>
2026-06-09 12:06:20 +0530

    fix: set unoptimized: true in production next.config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   images: {
-    path: isProd ? '/aestr/_next/image' : '/_next/image',
+    unoptimized: isProd,
     remotePatterns: [
       {
         protocol: 'https',
